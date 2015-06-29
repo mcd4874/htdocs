481c06e6fa257a1dd27f2096eb51469af287bead
mcd4874 <mcd4874@g.rit.edu>
2015-06-28 18:17:50 -0700

    Initial commit

---
+++ test.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <title>Shirt 4 Minh</title>
+</head>
+
+<body>
+    <h1>Shirt 4 Minh</h1>
+    <p>&copy  h Year 20<?php echo date('y'); ?> Shirt 4 Minh </p>
+</body>
+</html>
---
+++ xampp/perl.pl
@@ -0,0 +1,4 @@
+#!"D:\minh_stuff\learning_coding_online\XAMPPs\perl\bin\perl.exe"
+
+print "Content-Type: text/html\n\n";
+print "OK";
